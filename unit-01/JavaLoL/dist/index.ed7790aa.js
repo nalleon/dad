31b41aa640e94e5b2584c3af82ef8e2bab425d14
nalleon <nabil14716@gmail.com>
2024-10-07 19:38:41 +0100

    feat(JavaLoL): Continued css

--- unit-01/JavaLoL/dist/index.ed7790aa.js
+++ unit-01/JavaLoL/dist/index.ed7790aa.js
@@ -588,13 +588,23 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _championJs = require("./champion.js");
 var _championJsDefault = parcelHelpers.interopDefault(_championJs);
 var championsList = [];
-const button = document.querySelector("button");
-button.addEventListener("click", ()=>{
-    document.querySelector("#button").style.visibility = "hidden";
+const buttonIcons = document.getElementById("buttonIcons");
+const buttonFull = document.getElementById("buttonFull");
+buttonIcons.addEventListener("click", ()=>{
     document.querySelector("#champions").style.visibility = "visible";
-    getChampions();
+    hideButtons();
+    getChampionsIcons();
 });
-const getChampions = async ()=>{
+buttonFull.addEventListener("click", ()=>{
+    document.querySelector("#champions").style.visibility = "visible";
+    hideButtons();
+    getChampionsFull();
+});
+function hideButtons() {
+    buttonIcons.style.visibility = "hidden";
+    buttonFull.style.visibility = "hidden";
+}
+const getChampionsIcons = async ()=>{
     await fetch("https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json").then(function(result) {
         return result.json();
     }).then(function(result) {
@@ -607,30 +617,110 @@ const getChampions = async ()=>{
     });
     await showChampions();
 };
+const getChampionsFull = async ()=>{
+    await fetch("https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json").then(function(result) {
+        return result.json();
+    }).then(function(result) {
+        for(let championData in result.data){
+            let data = result.data[championData];
+            let champion = new (0, _championJsDefault.default)(data);
+            champion.setImg(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.id}_0.jpg`);
+            pushChampions(champion);
+        }
+    });
+    await showChampionsFullImg("full");
+};
 function pushChampions(champion) {
     championsList.push(champion);
 }
 const showChampions = async ()=>{
     const champions = document.getElementById("champions");
-    for(var i = 0; i < championsList.length; i++){
-        var aux = 0;
-        champions.innerHTML += `<div class="card">
-                                    <img src="${championsList[i].img}" onclick="">
-                                    <div class="info">
-                                        <h2>${championsList[i].name}</h2>
-                                        <div class="title">
-                                            <h3> ${championsList[i].title} </h3>
-                                        </div>    
-                                        <div class="tags">         
-                                                <div class="description">
-                                                ${championsList[i].description}
-                                                </div>
-                                        </div>
-                                        <div class="version">
-                                            ${championsList[i].version}
-                                        </div>
-                                    </div>
-                                </div>`;
+    champions.innerHTML = "";
+    for(var i = 0; i < championsList.length; i++)champions.innerHTML += `
+        <div class="card">
+            <img src="${championsList[i].img}" id="img-${i}">
+            <div class="info">
+                <h2>${championsList[i].name}</h2>
+                
+                <div class="tags">
+                    ${championsList[i].tags.join(" / ").toUpperCase()}
+                </div>
+                <div class="stats">
+                    <table>
+                        <tr>
+                            <th>ATK</th>
+                            <th>DEF</th>
+                            <th>MAG</th>
+                            <th>DIFF</th>
+                        </tr>
+                        <tr>
+                            <td>${championsList[i].attack}</td>
+                            <td>${championsList[i].defense}</td>
+                            <td>${championsList[i].magic}</td>
+                            <td>${championsList[i].difficulty}</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+                <div class="partype">
+                    ${championsList[i].partype}
+                </div>
+                <div class="version">
+                    ${championsList[i].version}
+                </div>
+            </div>
+        </div>`;
+};
+const showChampionsFullImg = async ()=>{
+    const champions = document.getElementById("champions");
+    champions.innerHTML = "";
+    for(var i = 0; i < championsList.length; i++)champions.innerHTML += `
+                    <div class="full-card">
+                        <img src="${championsList[i].img}" class="full-img" id="img-${i}">
+                        <div class="full-info">
+                            <h2>${championsList[i].name}</h2>
+                            <div class="title">
+                                <h3>${championsList[i].title}</h3>
+                            </div>
+                            <div class="description">
+                                <p>${championsList[i].description}</p>
+                            </div>    
+                        </div>
+                    </div>`;
+    changeSkin();
+};
+const changeSkin = ()=>{
+    championsList.forEach((champion, index)=>{
+        let skinIndex = 0;
+        const maxSkinPerChampion = 1;
+        const championImg = document.getElementById(`img-${index}`);
+        championImg.addEventListener("click", async ()=>{
+            skinIndex = (skinIndex + 1) % (maxSkinPerChampion + 1);
+            let skinUrl;
+            if (skinIndex === 0) skinUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`;
+            else skinUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_${skinIndex}.jpg`;
+            const imageExists = await checkImageExists(skinUrl);
+            if (imageExists) championImg.src = skinUrl;
+            else {
+                skinIndex = (skinIndex + 1) % (maxSkinPerChampion + 1);
+                const nextSkinUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_${skinIndex}.jpg`;
+                const nextImageExists = await checkImageExists(nextSkinUrl);
+                if (nextImageExists) championImg.src = nextSkinUrl;
+            }
+        });
+    });
+};
+/**
+     * Function to check if the skin of a champion exist
+     * @param {*} url 
+     * @returns true if exists, false otherwise
+     */ const checkImageExists = async (url)=>{
+    try {
+        const response = await fetch(url);
+        return response.ok;
+    } catch (error) {
+        console.error("Error while verifying img", error);
+        return false;
     }
 };
 
@@ -644,9 +734,13 @@ class Champion {
         this.version = data.version;
         this.title = data.title.toUpperCase();
         this.description = data.blurb.toUpperCase();
-        this.info = data.info;
         this.img = data.image.full;
         this.tags = data.tags;
+        this.partype = data.partype.toUpperCase();
+        this.attack = data.info.attack;
+        this.defense = data.info.defense;
+        this.magic = data.info.magic;
+        this.difficulty = data.info.difficulty;
     }
     setImg(splashUrl) {
         this.img = splashUrl;
