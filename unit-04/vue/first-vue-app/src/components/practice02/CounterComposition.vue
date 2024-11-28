6c7a00d1ba3a992cb903b7610b45a518b0103a88
nalleon <nabil14716@gmail.com>
2024-11-28 18:39:30 +0000

    feat(vue): Added bootstrap css to exercise

--- unit-04/vue/first-vue-app/src/components/practice02/CounterComposition.vue
+++ unit-04/vue/first-vue-app/src/components/practice02/CounterComposition.vue
@@ -1,30 +1,46 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-            <h2> Composition </h2>
-            <h3>{{ counter }} </h3>
-            <input type="number" name="counter" v-model="counter">
-        </div>
-    </div>
-    <br>
-    <br>
-    <div class="row">
-        <div class="col-3">
-            <button class="btn btn-success" @click="addCounter">Incrementar</button>
-        </div>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header mb-3">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="fw-bold text-uppercase text-center text-primary"> Composition </h2>
+                        <h3 class="fw-bold text-uppercase text-center">{{ counter }} </h3>
+                        <input type="number" name="counter" v-model="counter" class="form-control-md form-control">
+                    </div>
+                </div>
+            </div>
 
-        <div class="col-3">
-            <button class="btn btn-warning" @click="minimizeCounter">Decrementar</button>
-        </div>
-        <div class="col-3">
-            <button class="btn btn-danger" @click="duplicateCounter">Duplicar</button>
-        </div>
-        
-        <div class="col-3">
-            <button class="btn btn-primary" @click="resetCounter">Reiniciar</button>
-        </div>
-        <div class="col-12">
-            <div v-if="counter >= 10">¡El valor de {{counter}} es alto!</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-success text-light fw-bold" @click="addCounter">Incrementar</button>
+                    </div>
+
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-warning text-light fw-bold" @click="minimizeCounter">Decrementar</button>
+                    </div>
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-danger text-light fw-bold" @click="duplicateCounter">Duplicar</button>
+                    </div>
+                    
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-primary text-light fw-bold" @click="resetCounter">Reiniciar</button>
+                    </div>
+
+                    <div v-if="counter >= 10">
+                        <div class="col-12 mb-4 mt-5">
+                            <div class="fw-bold text-uppercase text-center">
+                                <h3>¡El valor de  
+                                    <span class="text-danger">{{counter}}</span>
+                                    es alto!
+                                </h3>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- unit-04/vue/first-vue-app/src/components/practice02/CounterOption.vue
+++ unit-04/vue/first-vue-app/src/components/practice02/CounterOption.vue
@@ -1,16 +1,48 @@
 <template>
-    <h2>Option</h2>
-    <h3>{{ counter }} </h3>
-    <input type="number" name="counter" v-model="counter">
-    <br>
-    <br>
-    <button @click="addCounter">Incrementar</button>
-    <button @click="minimizeCounter">Decrementar</button>
-    <button @click="duplicateCounter">Duplicar</button>
-    <button @click="resetCounter">Reiniciar</button>
-    <br>
-    <div v-if="counter >= 10">¡El valor de {{counter}} es alto!</div>
-    <br>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header mb-3">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="fw-bold text-uppercase text-center text-primary"> Option </h2>
+                        <h3 class="fw-bold text-uppercase text-center">{{ counter }} </h3>
+                        <input type="number" name="counter" v-model="counter" class="form-control-md form-control">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-success text-light fw-bold" @click="addCounter">Incrementar</button>
+                    </div>
+
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-warning text-light fw-bold" @click="minimizeCounter">Decrementar</button>
+                    </div>
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-danger text-light fw-bold" @click="duplicateCounter">Duplicar</button>
+                    </div>
+                    
+                    <div class="col-12 mb-4 col-md-3">
+                        <button class="btn w-75 btn-primary text-light fw-bold" @click="resetCounter">Reiniciar</button>
+                    </div>
+
+                    <div v-if="counter >= 10">
+                        <div class="col-12 mb-4 mt-5">
+                            <div class="fw-bold text-uppercase text-center">
+                                <h3>¡El valor de  
+                                    <span class="text-danger">{{counter}}</span>
+                                    es alto!
+                                </h3>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
